ad9b2b672ded7fef168b71d4cfb5a6585da86f6c
JonBabao <jonendrickb@gmail.com>
2025-03-05 00:47:54 +0800

    Create depoly-commands.js

---
+++ deploy-commands.js
@@ -0,0 +1,54 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { config } from 'dotenv';
+config()
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+let TOKEN = process.env.DISCORD_TOKEN
+let CLIENT_ID = process.env.CLIENT_ID
+let GUILD_ID = process.env.GUILD_ID
+
+import { REST, Routes } from 'discord.js'
+import * as fs from 'fs'
+import * as path from 'path'
+
+const commands = [];
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = `file://${path.join(commandsPath, file)}`;
+		const command = await import(filePath);
+		if ('data' in command && 'execute' in command) {
+			commands.push(command.data.toJSON());
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+// Construct and prepare an instance of the REST module
+const rest = new REST().setToken(TOKEN);
+
+// and deploy your commands!
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+})();
