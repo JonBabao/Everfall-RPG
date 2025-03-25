f40cf9fb9207b92d131b9c989a4e840a726c2786
JonBabao <jonendrickb@gmail.com>
2025-03-25 23:50:07 +0800

    Create ping.js for ping command

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,12 +1,13 @@
 import { SlashCommandBuilder } from 'discord.js';
 
+const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 export const data = new SlashCommandBuilder()
     .setName('ping')
-    .setDescription('Replies with Pong!')
+    .setDescription('Replies with Pong!');
 
 export const execute = async (interaction) => {
-    await interaction.reply({ 
-        content: "Pong"
-    });
-}
+    await interaction.deferReply();
+    await wait(4000); 
+    await interaction.editReply('Pong!');
+};
